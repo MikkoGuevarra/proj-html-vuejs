256bacd806d0390be32426df50590d420c309dfd
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-30 12:32:18 +0100

    scaffolding

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 var app = new Vue({
     el : '#root',
     data: {
-
+        navMenu: ['Home', 'About us', 'Feature', 'Contact US']
     },
-    mounted() {
-        alert('123 check!!')
-    }
 });
