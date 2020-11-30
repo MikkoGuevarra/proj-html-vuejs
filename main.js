f347661db41eabd9e3001df0243df839560b77a7
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-30 15:49:10 +0100

    jumbotron

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var app = new Vue({
     el : '#root',
     data: {
-        navMenu: ['Home', 'About us', 'Feature', 'Contact US']
+        navMenu: ['Home', 'About us', 'Feature', 'Contact US'],
+        title: 'The biggest platform for your growth',
+        description: 'There are many variations of passages of Lorem Ipsum avalable, but the majority have suffered alteration in some form'
     },
 });
