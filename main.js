bb5c25fef7942c131b4c5275acbf62cea73b75a3
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-30 16:36:19 +0100

    done with section 1

--- main.js
+++ main.js
@@ -3,6 +3,41 @@ var app = new Vue({
     data: {
         navMenu: ['Home', 'About us', 'Feature', 'Contact US'],
         title: 'The biggest platform for your growth',
-        description: 'There are many variations of passages of Lorem Ipsum avalable, but the majority have suffered alteration in some form'
+        description: 'There are many variations of passages of Lorem Ipsum avalable, but the majority have suffered alteration in some form',
+        marketings: [
+            {
+                title: 'Online Marketing',
+                photo_path: 'img/24.png',
+                desc: 'It is a long established fact that a reader will be distracted',
+                selection: [
+                    'SEO',
+                    'SEM',
+                    'Web Strategy',
+                    'Social management'
+                ]
+            },
+            {
+                title: 'Online Marketing',
+                photo_path: 'img/24.png',
+                desc: 'It is a long established fact that a reader will be distracted',
+                selection: [
+                    'SEO',
+                    'SEM',
+                    'Web Strategy',
+                    'Social management'
+                ]
+            },
+            {
+                title: 'Online Marketing',
+                photo_path: 'img/24.png',
+                desc: 'It is a long established fact that a reader will be distracted',
+                selection: [
+                    'SEO',
+                    'SEM',
+                    'Web Strategy',
+                    'Social management'
+                ]
+            }
+        ]
     },
 });
